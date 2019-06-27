bc9bcccd47013e33261a9683639da760f151bada
vinsonw <me@vinsonwei.com>
2019-06-27 20:42:07 +0800

    ignore .idea folder by .gitignore

--- fishbook.py
+++ fishbook.py
@@ -2,6 +2,7 @@
 __author__ = 'Vinson'
 
 from flask import Flask, make_response
+from helper import is_isbn_or_key
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,16 +13,14 @@ def search(q, page):
     """
         q 代表普通关键字或者isbn
         page
+        # isbn
+        # isbn13, 13个0到9的数字
+        # isbn10, 10个0到9数字，含有'-'
     """
-    # isbn
-    # isbn13, 13个0到9的数字
-    # isbn10, 10个0到9数字，含有'-'
-    isbn_or_key = 'key'
-    if len(q) == 13 and q.isdigit():
-        isbn_or_key = 'isbn'
-    short_q = q.replace('-', '')
-    if '-' in q and len(short_q) == 10 and short_q.isdigit():
-        isbn_or_key = 'isbn'
+
+    isbn_or_key = is_isbn_or_key(q)
+
+
 
 
 
