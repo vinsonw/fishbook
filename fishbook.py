64b82e3063ed9ec22b13fd0d4d2884923ef73dc4
vinsonw <me@vinsonwei.com>
2019-06-27 20:00:03 +0800

    skeleton

--- fishbook.py
+++ fishbook.py
@@ -7,19 +7,14 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route('/hello')
-def hello():
-    headers = {
-        'content-type': 'application/json',
-        'location': 'http://www.bing.com'
-    }
-    # response = make_response('<html>i</html>', 301)
-    # response.headers = headers
-    # return response
-    return '<html></html>', 301, headers
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+        q 代表普通关键字或者isbn
+        page
 
+    """
 
-# app.add_url_rule('/hello', view_func=hello)
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], port=81)
