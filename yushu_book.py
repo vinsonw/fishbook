b24edf361ec48f258a429b228ac83c837c9bbdf7
vinsonw <me@vinsonwei.com>
2019-06-28 03:19:28 +0800

    fix typo

--- yushu_book.py
+++ yushu_book.py
@@ -7,7 +7,7 @@ from httper import HTTP
 
 class YuShuBook:
     isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
-    keyword_rul = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
+    keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
     @classmethod
     def search_by_isbn(cls, isbn):
@@ -17,7 +17,7 @@ class YuShuBook:
 
     @classmethod
     def search_by_keyword(cls, keyword, count=15, start=0):
-        url = cls.keyword_rul.format(keyword, count, start)
+        url = cls.keyword_url.format(keyword, count, start)
         result = HTTP.get(url)
         return result
 
