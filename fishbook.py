a88f68735ccd72fdf877d769a305c4864d03f194
vinsonw <me@vinsonwei.com>
2019-06-27 20:22:47 +0800

    how to determine whether the key word 'q' is isbn or not.

--- fishbook.py
+++ fishbook.py
@@ -12,8 +12,17 @@ def search(q, page):
     """
         q 代表普通关键字或者isbn
         page
-
     """
+    # isbn
+    # isbn13, 13个0到9的数字
+    # isbn10, 10个0到9数字，含有'-'
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+    short_q = q.replace('-', '')
+    if '-' in q and len(short_q) == 10 and short_q.isdigit():
+        isbn_or_key = 'isbn'
+
 
 
 if __name__ == '__main__':
