afacf64953f614db77b56fb09b74daad7e58c515
vinsonw <me@vinsonwei.com>
2019-06-28 01:30:33 +0800

    after rewrite if condition

--- http.py
+++ http.py
@@ -10,9 +10,8 @@ import requests
 class HTTP:
     def get(self, url, return_json=True):
         r = requests.get(url)
-        if return_json:
-            return r.json()
-        else:
-            return r.text
+        if r.status_code != 200:
+            return {} if return_json else ''
+        return r.json() if return_json else r.text
 
 
