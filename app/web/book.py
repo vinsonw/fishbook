8337f8a36d18d6ac7f5dc8a75db03061c36f127e
vinsonw <me@vinsonwei.com>
2019-06-30 03:52:05 +0800

    upgrade IDE from Pycharm Community to Professional

--- app/web/book.py
+++ app/web/book.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # 2019/6/28 18:51
-from flask import jsonify
+from flask import jsonify, request
 
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
@@ -10,8 +10,8 @@ __author__ = 'Vinson <me@vinsonwei.com>'
 
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
     """
         q 代表普通关键字或者isbn
         page
@@ -19,6 +19,17 @@ def search(q, page):
         # isbn13, 13个0到9的数字
         # isbn10, 10个0到9数字，含有'-'
     """
+    # Request Response
+    # HTTP 的请求信息
+    # 查询参数 POST参数 remote ip
+
+    q = request.args['q']
+    page = request.args['page']
+
+    a = request.args.to_dict() # 只有Pycharm Professional版本才有request对象的方法成员提示
+
+    # a = request.
+
 
     isbn_or_key = is_isbn_or_key(q)
 
