a974f5b9931b086ac8471198aa4884fd4a90327c
vinsonw <me@vinsonwei.com>
2019-06-30 22:30:58 +0800

    save before chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # 2019/6/29 21:31
 from flask import Flask
-
+from app.models.book import db
 
 __author__ = 'Vinson <me@vinsonwei.com>'
 
@@ -9,7 +9,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('app.secure')
     app.config.from_object('app.setting')
+
     register_blueprint(app)
+    db.init_app(app)
+    db.create_all(app=app)
+
     return app
 
 def register_blueprint(app):
--- app/secure.py
+++ app/secure.py
@@ -2,4 +2,4 @@
 # 避免加入git push
 
 DEBUG = True
-
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:secret@localhost:3306/fisher'
